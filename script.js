9eda1f81eda04c88992773dd4fb354d3246e03ed
Joshua O'Brien <josh.o@canva.com>
2019-10-29 01:16:41 +1100

    Change shape; reposition; add rotation on keypress

--- script.js
+++ script.js
@@ -5,20 +5,62 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 1, 2);
 const material = new THREE.MeshBasicMaterial({ color: 0x2a3670 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 4;
+
+cube.rotation.x = 1.45;
+cube.rotation.y = -0.6;
+
 
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  Math.floor(Math.random() * Math.floor(10))
+
+  processKeys();
 
   renderer.render(scene, camera);
 }
+
+function processKeys() {
+  currentlyPressed.forEach(key => {
+    switch (key) {
+      case 40:
+        console.log(cube.rotation.x);
+        cube.rotation.x += 0.01;
+        break;
+      case 38:
+        cube.rotation.x -= 0.01;
+        break;
+      case 39:
+        cube.rotation.y += 0.01;
+        break;
+      case 37:
+        cube.rotation.y -= 0.01;
+        break;
+    }
+  })
+
+  console.table({
+    x: cube.rotation.x,
+    y: cube.rotation.y,
+  });
+}
+
+const currentlyPressed = new Set();
+
+document.addEventListener('keyup', e => {
+  currentlyPressed.delete(e.keyCode);
+})
+
+document.addEventListener('keydown', (e) => {
+  currentlyPressed.add(e.keyCode);
+})
 
 animate();
